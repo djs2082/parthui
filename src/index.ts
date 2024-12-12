65349d13fe55436896f8aee6c8a0d3b22a742cc7
dilipjoshis <dilip@higgsbosonhealth.com>
2024-12-12 17:20:01 +0530

    corrected import of SpotifyPlayer

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { PrimaryButton, SecondaryButton } from "./components/Button";
 export { Modal } from "./components/Modal";
+export { SpotifyPlayer } from "./components/SpotifyPlayer";
 export { BasicLoader, LoveLoader } from "./components/Loader";
 export { useS3FileManager } from "./hooks";
 export { useSpotifyPlayer } from "./hooks";
